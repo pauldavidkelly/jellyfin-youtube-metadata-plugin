0cab6cd8acf159fa13946be87f1b989852617655
Paul Kelly <paul@kelly.ky>
2021-02-06 16:59:50 +0000

    Added Utils class for utility functions required across all of the providers

---
+++ Jellyfin.Plugin.YoutubeMetadata/YTUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Jellyfin.Plugin.YoutubeMetadata.YTTools
+{
+    public static class YTUtils
+    {
+        public const string BaseUrl = "https://m.youtube.com/";
+        public const string YTID_RE = @"(?<=\[)[a-zA-Z0-9\-_]{11}(?=\])";
+
+        /// <summary>
+        ///  Returns the Youtube ID from the file path. Matches last 11 character field inside square brackets.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetYTID(string name)
+        {
+            var match = Regex.Match(name, YTID_RE);
+            return match.Value;
+        }
+    }
+}
